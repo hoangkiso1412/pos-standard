fbc53748fe7a5b5c1323e6486cf78b50b1771fc8
limengkruy <kruylimeng@yahoo.com>
2019-10-20 23:52:56 +0700

    Upload Project

---
+++ application/config/define_pre.php
@@ -0,0 +1,4 @@
+<?php
+define('SERVICE', 'http://security.reahou.com/check');
+define('APPVER', '?v=5.0');
+define('UPDATE_SERVICE', 'http://provider.ultimatekode.com/updatesss/');// change it to on for exclusive
